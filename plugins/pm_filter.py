fafb5e2962ced832d345d382c418163854f2f3a9
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2023-08-12 20:56:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1409,14 +1409,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('FIʟᴛᴇʀs', callback_data='filters'),
             InlineKeyboardButton('Fɪʟᴇ Sᴛᴏʀᴇ', callback_data='store_file'),  
-            InlineKeyboardButton('𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙿𝙷', callback_data='TELE_TXT')
+            InlineKeyboardButton('𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙿𝙷', callback_data='tele')
         ], [
             InlineKeyboardButton('Cᴏɴɴᴇᴄᴛɪᴏɴ', callback_data='coct'),
             InlineKeyboardButton('Exᴛʀᴀ Features', callback_data='extra'), 
-            InlineKeyboardButton('𝚈𝚃-𝙳𝙻', callback_data='YTDL')
+            InlineKeyboardButton('𝚈𝚃-𝙳𝙻', callback_data='ytdl')
         ], [
             InlineKeyboardButton('Hᴏᴍᴇ', callback_data='start'),
-            InlineKeyboardButton('Sᴛᴀᴛᴜs', callback_data='stats'), 
             InlineKeyboardButton('𝚂𝙷𝙰𝚁𝙴-𝚃𝙴𝚇𝚃', callback_data='SHARE_TXT')
         ], [
             InlineKeyboardButton('💠 𝚂𝚃𝙰𝚃𝚄𝚂 💠', callback_data='stats')
